9359761a75b0d195caecd87e118f31ad54839d04
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-09-01 17:30:15 -0400

    add tests

--- packages/svelte/src/index.spec.js
+++ packages/svelte/src/index.spec.js
@@ -1,4 +1,4 @@
-import { htmlLoader } from './index';
+import { htmlLoader, errorHtmlLoader } from './index';
 import Component from '../test-helpers/compiledComponent';
 
 describe('render HTML', () => {
@@ -6,7 +6,6 @@ describe('render HTML', () => {
     const res = {
       locals: {},
     };
-
     await htmlLoader(
       {
         query: {},
@@ -34,5 +33,49 @@ describe('render HTML', () => {
               window.__SSR_AAS_LOAD_PROPS__[3] = {};
           </script>
       `);
+  });
+  it('should set an error', async () => {
+    const res = {
+      locals: {},
+    };
+    await htmlLoader(
+      {
+        query: {},
+        params: {
+          component: 'Component.svelte',
+        },
+        id: 3,
+        locals: {
+          component: 'component',
+          componentProps: {},
+          ResolvedComponent: {},
+        },
+      },
+      res,
+      () => null,
+    );
+    expect(res.locals.error).not.toBeUndefined();
+  });
+});
+
+describe('handle svelte rendering error', () => {
+  it('should return error html', async () => {
+    const res = {
+      locals: {},
+    };
+    await errorHtmlLoader(
+      {
+        locals: {
+          component: 'component',
+          componentProps: {},
+          error: {
+            message: 'test error message',
+          },
+        },
+      },
+      res,
+      () => null,
+    );
+    expect(res.locals.html).not.toBeUndefined();
   });
 });
