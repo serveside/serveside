1c9f0bf244e56785fe4ef072ca74b30b5c51af86
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-09-01 17:30:10 -0400

    fix name in svelte package jest config

--- packages/svelte/jest.config.js
+++ packages/svelte/jest.config.js
@@ -2,7 +2,7 @@ const base = require('../../jest.config.base.js');
 
 module.exports = {
   ...base,
-  name: '@serveside/ssr-aas-core',
+  name: '@serveside/ssr-aas-svelte',
   displayName: 'Svelte',
   rootDir: '.',
   roots: ['<rootDir>/src'],
